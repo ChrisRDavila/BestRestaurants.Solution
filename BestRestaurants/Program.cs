fa49c05736d6a0ca0a6325b77ef5b982c7ad47f1
ChrisRDavila <crossdavila@gmail.com>
2023-05-17 11:28:30 -0700

    Adding all files for start
    Co-authored-by: MarcusKyung <MarcusKyung@users.noreply.github.com>

--- BestRestaurants/Program.cs
+++ BestRestaurants/Program.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using ProjectName.Models;
+using BestRestaurants.Models;
 
-namespace ProjectName
+namespace BestRestaurants
 {
   class Program
   {
@@ -14,7 +14,7 @@ namespace ProjectName
 
       builder.Services.AddControllersWithViews();
 
-      builder.Services.AddDbContext<ProjectNameContext>(
+      builder.Services.AddDbContext<BestRestaurantsContext>(
                         dbContextOptions => dbContextOptions
                           .UseMySql(
                             builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
@@ -37,5 +37,4 @@ namespace ProjectName
       app.Run();
     }
   }
-}
 }
